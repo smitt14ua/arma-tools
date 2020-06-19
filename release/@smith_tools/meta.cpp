4a9f357317034ef7284e4e54194fc47573914496
Smith <smitt14ua@gmail.com>
2020-06-19 21:28:07 +0200

    Ignore release folder

--- release/@smith_tools/meta.cpp
+++
@@ -1,4 +0,0 @@
-protocol = 1;
-publishedid = 1948449854;
-name = "Smith tools";
-timestamp = 5248870202104406904;
--- release/@smith_tools/mod.cpp
+++
@@ -1,7 +0,0 @@
-name = $STR_tools_main_name;
-overview = $STR_tools_main_description;
-action = "https://steamcommunity.com/sharedfiles/filedetails/?id=1948449854";
-logoSmall = "data\logo_small_ca.paa";
-logo = "data\logo_ca.paa";
-logoOver = "data\logo_ca.paa";
-picture  = "data\picture_ca.paa";
